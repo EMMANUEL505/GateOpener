29f09e46e8bbe47f693e1e3ae14b24bfe0286de3
EMMANUEL505 <EMMANUELSIGALA@HOTMAIL.COM>
2018-11-12 13:00:21 -0700

    V1.4 Language translation
    -Changed to spanish
    -Sms init notification disabled

--- build/default/debug/eeprom.p1.d
+++
@@ -1,8 +0,0 @@
- build/default/debug/eeprom.d  \
- build/default/debug/eeprom.p1:  \
- eeprom.c  \
-I2C.h  \
-general.h  \
-usart.h  \
-gpio.h  \
-eeprom.h 
--- build/default/debug/gpio.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/debug/gpio.d  \
- build/default/debug/gpio.p1:  \
- gpio.c  \
-general.h  \
-gpio.h 
